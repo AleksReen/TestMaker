755633cf8c01397056d76c56a9e9212d4586c512
MINSK\Aliaksandr_Reyentovi <Aliaksandr_Reyentovich@epam.com>
2018-07-25 16:38:19 +0300

    added logic for QuizController

--- TestMakerWebApp/Controllers/QuizController.cs
+++ TestMakerWebApp/Controllers/QuizController.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TestMaker.Models.ViewModels;
+using TestMakerWebApp.Helpers.DataHelper;
 
 namespace TestMakerWebApp.Controllers
 {
     [Route("api/[controller]")]
     public class QuizController : Controller
     {
-        
+        [HttpGet("Latest/{num}")]
+        public IActionResult Latest(int num = 10) => new JsonResult(QuizDataHelper.GetQuizViewModelsList(num), QuizDataHelper.JsonSettings);
+
+        [HttpGet("ByTitle/{num:int?}")]
+        public IActionResult ByTitle(int num = 10) => new JsonResult(QuizDataHelper.GetQuizViewModelsList(num).OrderBy(x => x.Title), QuizDataHelper.JsonSettings);
+
+        [HttpGet("Random/{num:int?}")]
+        public IActionResult Random(int num = 10) => new JsonResult(QuizDataHelper.GetQuizViewModelsList(num).OrderBy(x => Guid.NewGuid()), QuizDataHelper.JsonSettings);
     }
 }
---
+++ TestMakerWebApp/Helpers/DataHelper/QuizDataHelper.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestMaker.Models.ViewModels;
+
+namespace TestMakerWebApp.Helpers.DataHelper
+{
+    public static class QuizDataHelper
+    {
+        public static JsonSerializerSettings JsonSettings { get; set; } = new JsonSerializerSettings { Formatting = Formatting.Indented };
+
+        public static List<QuizViewModel> GetQuizViewModelsList(int num) {
+
+            var sampleQuizzes = new List<QuizViewModel>() {
+                new QuizViewModel {
+                    Id = 1,
+                    Title =  "Which Shingeki No Kyojin character are you?",
+                    Description =  "Anime-related personality test",
+                    CreateDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                }
+            };
+
+            for (int i = 2; i <= num; i++)
+            {
+                sampleQuizzes.Add(new QuizViewModel()
+                {
+                    Id = i,
+                    Title = $"Sample Quiz {i}",
+                    Description = "This is a sample quiz",
+                    CreateDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+
+            return sampleQuizzes;
+        }
+    }
+}
