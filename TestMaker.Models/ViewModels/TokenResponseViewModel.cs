e1d241e18973c72630b36807149ee444cafcdf5e
MINSK\Aliaksandr_Reyentovi <Aliaksandr_Reyentovich@epam.com>
2018-08-15 15:44:35 +0300

    Added Token Infrastructure

---
+++ TestMaker.Models/ViewModels/TokenResponseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TestMaker.Models.ViewModels
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    class TokenResponseViewModel
+    {
+        public TokenResponseViewModel()
+        {
+
+        }
+
+        public string token { get; set; }
+        public int expiration { get; set; }
+    }
+}
---
+++ TestMakerWebApp/Controllers/TokenController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using TestMaker.Data.Context;
+using TestMaker.Models.Data;
+
+namespace TestMakerWebApp.Controllers
+{
+    public class TokenController : BaseApiController
+    {
+        public TokenController(
+            ApplicationDbContext dbContext,        
+            RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager,
+            IConfiguration configuration)
+            : base(dbContext, roleManager, userManager, configuration) {}
+
+    }
+}
