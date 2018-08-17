3293aa6c483089726b8af442f34ce86b4877c81e
MINSK\Aliaksandr_Reyentovi <Aliaksandr_Reyentovich@epam.com>
2018-08-17 16:41:19 +0300

    added auth interceptor

--- TestMakerWebApp/ClientApp/src/app/app.module.ts
+++ TestMakerWebApp/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 //Services
@@ -10,6 +10,7 @@ import { DataQuestionService } from './services/data-question.service';
 import { DataAnswerService } from './services/data-answer.service';
 import { DataResultService } from './services/data-result.service';
 import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './services/auth-interceptor';
 
 //Components
 import { AppComponent } from './app.component';
@@ -70,7 +71,17 @@ import { QuizSearchComponent } from './quiz-search/quiz-search.component';
       { path: '**', component: PageNotFoundComponent }
     ])
   ],
-  providers: [DataQuizService, DataQuestionService, DataAnswerService, DataResultService, AuthService],
+  providers: [
+    DataQuizService,
+    DataQuestionService,
+    DataAnswerService,
+    DataResultService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi:true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
